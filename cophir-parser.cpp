c66adef2c6b7673416bb140dc24d201174a2ef79
PH <oliveira.ph17@gmail.com>
2015-03-16 03:19:20 -0300

    Changes the structure of the algorithm in order to consider that, depending on the XML file, the MPEG-7 descriptors may be in different positions within the file.

--- cophir-parser.cpp
+++ cophir-parser.cpp
@@ -3,7 +3,7 @@
 #include <fstream>
 #include "strtk.hpp"
 
-#define XML_FILES 1000000
+#define XML_FILES 10000000
 
 using namespace std;
 
@@ -45,288 +45,325 @@ int main() {
         // Opens current XML file.
         ifstream xml_file(line.c_str());
 
-        string coeff_value;
-        string yac_coeff5_value;
-        string cbac_coeff2_value;
-        string crac_coeff2_value;
-        string values_value;
-        string bin_counts_value;
-        string energy_value;
-        string energy_deviation_value;
+        bool found_coeff = false;
 
-        ////////////////////////////////////////////////////////////////////////////////////////
-        // Scalable Color.
-        ////////////////////////////////////////////////////////////////////////////////////////
+        bool found_ydc_coeff = false;
+        bool found_cbdc_coeff = false;
+        bool found_crdc_coeff = false;
+        bool found_yac_coeff5 = false;
+        bool found_cbac_coeff2 = false;
+        bool found_crac_coeff2 = false;
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<Coeff>");
+        bool found_values = false;
 
-            if (pos != string::npos) {
-                coeff_value = line.substr(7, line.length() - 15);
+        bool found_bin_counts = false;
 
-                break;
-            }
-        }
-
-        tokenizer_type coeff_tokenizer(coeff_value,
-                                       predicate,
-                                       strtk::tokenize_options::compress_delimiters);
-
-        tokenizer_type::iterator itr = coeff_tokenizer.begin();
-
-        while (itr != coeff_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
-            }
-
-            ++itr;
-        }
-
-        ////////////////////////////////////////////////////////////////////////////////////////
-        // Color Layout.
-        ////////////////////////////////////////////////////////////////////////////////////////
+        bool found_average = false;
+        bool found_standard_deviation = false;
+        bool found_energy = false;
+        bool found_energy_deviation = false;
 
         while (getline(xml_file, line)) {
-            pos = line.find("<YDCCoeff>");
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Scalable Color.
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-            if (pos != string::npos) {
-                input << line.substr(10, line.length() - 21);
-                input << " ";
+            if (!found_coeff) {
+                pos = line.find("<Coeff>");
 
-                break;
-            }
-        }
+                if (pos != string::npos) {
+                    string value = line.substr(7, line.length() - 15);
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<CbDCCoeff>");
-
-            if (pos != string::npos) {
-                input << line.substr(11, line.length() - 23);
-                input << " ";
-
-                break;
-            }
-        }
-
-        while (getline(xml_file, line)) {
-            pos = line.find("<CrDCCoeff>");
-
-            if (pos != string::npos) {
-                input << line.substr(11, line.length() - 23);
-                input << " ";
-
-                break;
-            }
-        }
-
-        while (getline(xml_file, line)) {
-            pos = line.find("<YACCoeff5>");
-
-            if (pos != string::npos) {
-                yac_coeff5_value = line.substr(11, line.length() - 23);
-
-                break;
-            }
-        }
-
-        tokenizer_type yac_coeff5_tokenizer(yac_coeff5_value,
-                                            predicate,
-                                            strtk::tokenize_options::compress_delimiters);
-
-        itr = yac_coeff5_tokenizer.begin();
-
-        while (itr != yac_coeff5_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
-            }
-
-            ++itr;
-        }
-
-        while (getline(xml_file, line)) {
-            pos = line.find("<CbACCoeff2>");
-
-            if (pos != string::npos) {
-                cbac_coeff2_value = line.substr(12, line.length() - 25);
-
-                break;
-            }
-        }
-
-        tokenizer_type cbac_coeff2_tokenizer(cbac_coeff2_value,
+                    tokenizer_type tokenizer(value,
                                              predicate,
                                              strtk::tokenize_options::compress_delimiters);
 
-        itr = cbac_coeff2_tokenizer.begin();
+                    tokenizer_type::iterator itr = tokenizer.begin();
 
-        while (itr != cbac_coeff2_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
+
+                        ++itr;
+                    }
+
+                    found_coeff = true;
+
+                    continue;
+                }
             }
 
-            ++itr;
-        }
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Color Layout.
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<CrACCoeff2>");
+            if (!found_ydc_coeff) {
+                pos = line.find("<YDCCoeff>");
 
-            if (pos != string::npos) {
-                crac_coeff2_value = line.substr(12, line.length() - 25);
+                if (pos != string::npos) {
+                    input << line.substr(10, line.length() - 21);
+                    input << " ";
 
-                break;
+                    found_ydc_coeff = true;
+
+                    continue;
+                }
             }
-        }
 
-        tokenizer_type crac_coeff2_tokenizer(crac_coeff2_value,
+            if (!found_cbdc_coeff) {
+                pos = line.find("<CbDCCoeff>");
+
+                if (pos != string::npos) {
+                    input << line.substr(11, line.length() - 23);
+                    input << " ";
+
+                    found_cbdc_coeff = true;
+
+                    continue;
+                }
+            }
+
+            if (!found_crdc_coeff) {
+                pos = line.find("<CrDCCoeff>");
+
+                if (pos != string::npos) {
+                    input << line.substr(11, line.length() - 23);
+                    input << " ";
+
+                    found_crdc_coeff = true;
+
+                    continue;
+                }
+            }
+
+            if (!found_yac_coeff5) {
+                pos = line.find("<YACCoeff5>");
+
+                if (pos != string::npos) {
+                    string value = line.substr(11, line.length() - 23);
+
+                    tokenizer_type tokenizer(value,
                                              predicate,
                                              strtk::tokenize_options::compress_delimiters);
 
-        itr = crac_coeff2_tokenizer.begin();
+                    tokenizer_type::iterator itr = tokenizer.begin();
 
-        while (itr != crac_coeff2_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
+
+                        ++itr;
+                    }
+
+                    found_yac_coeff5 = true;
+
+                    continue;
+                }
             }
 
-            ++itr;
-        }
+            if (!found_cbac_coeff2) {
+                pos = line.find("<CbACCoeff2>");
 
-        ////////////////////////////////////////////////////////////////////////////////////////
-        // Color Structure.
-        ////////////////////////////////////////////////////////////////////////////////////////
+                if (pos != string::npos) {
+                    string value = line.substr(12, line.length() - 25);
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<Values>");
+                    tokenizer_type tokenizer(value,
+                                             predicate,
+                                             strtk::tokenize_options::compress_delimiters);
 
-            if (pos != string::npos) {
-                values_value = line.substr(8, line.length() - 17);
+                    tokenizer_type::iterator itr = tokenizer.begin();
 
-                break;
-            }
-        }
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
 
-        tokenizer_type values_tokenizer(values_value,
-                                        predicate,
-                                        strtk::tokenize_options::compress_delimiters);
+                        ++itr;
+                    }
 
-        itr = values_tokenizer.begin();
+                    found_cbac_coeff2 = true;
 
-        while (itr != values_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
+                    continue;
+                }
             }
 
-            ++itr;
-        }
+            if (!found_crac_coeff2) {
+                pos = line.find("<CrACCoeff2>");
 
-        ////////////////////////////////////////////////////////////////////////////////////////
-        // Edge Histogram.
-        ////////////////////////////////////////////////////////////////////////////////////////
+                if (pos != string::npos) {
+                    string value = line.substr(12, line.length() - 25);
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<BinCounts>");
+                    tokenizer_type tokenizer(value,
+                                             predicate,
+                                             strtk::tokenize_options::compress_delimiters);
 
-            if (pos != string::npos) {
-                bin_counts_value = line.substr(11, line.length() - 23);
+                    tokenizer_type::iterator itr = tokenizer.begin();
 
-                break;
-            }
-        }
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
 
-        tokenizer_type bin_counts_tokenizer(bin_counts_value,
-                                            predicate,
-                                            strtk::tokenize_options::compress_delimiters);
+                        ++itr;
+                    }
 
-        itr = bin_counts_tokenizer.begin();
+                    found_crac_coeff2 = true;
 
-        while (itr != bin_counts_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
+                    continue;
+                }
             }
 
-            ++itr;
-        }
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Color Structure.
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-        ////////////////////////////////////////////////////////////////////////////////////////
-        // Homogeneous Texture.
-        ////////////////////////////////////////////////////////////////////////////////////////
+            if (!found_values) {
+                pos = line.find("<Values>");
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<Average>");
+                if (pos != string::npos) {
+                    string value = line.substr(8, line.length() - 17);
 
-            if (pos != string::npos) {
-                input << line.substr(9, line.length() - 19);
-                input << " ";
+                    tokenizer_type tokenizer(value,
+                                             predicate,
+                                             strtk::tokenize_options::compress_delimiters);
 
-                break;
-            }
-        }
+                    tokenizer_type::iterator itr = tokenizer.begin();
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<StandardDeviation>");
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
 
-            if (pos != string::npos) {
-                input << line.substr(19, line.length() - 39);
-                input << " ";
+                        ++itr;
+                    }
 
-                break;
-            }
-        }
+                    found_values = true;
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<Energy>");
-
-            if (pos != string::npos) {
-                energy_value = line.substr(8, line.length() - 17);
-
-                break;
-            }
-        }
-
-        tokenizer_type energy_tokenizer(energy_value,
-                                        predicate,
-                                        strtk::tokenize_options::compress_delimiters);
-
-        itr = energy_tokenizer.begin();
-
-        while (itr != energy_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
+                    continue;
+                }
             }
 
-            ++itr;
-        }
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Edge Histogram.
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-        while (getline(xml_file, line)) {
-            pos = line.find("<EnergyDeviation>");
+            if (!found_bin_counts) {
+                pos = line.find("<BinCounts>");
 
-            if (pos != string::npos) {
-                energy_deviation_value = line.substr(17, line.length() - 35);
+                if (pos != string::npos) {
+                    string value = line.substr(11, line.length() - 23);
 
-                break;
+                    tokenizer_type tokenizer(value,
+                                             predicate,
+                                             strtk::tokenize_options::compress_delimiters);
+
+                    tokenizer_type::iterator itr = tokenizer.begin();
+
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
+
+                        ++itr;
+                    }
+
+                    found_bin_counts = true;
+
+                    continue;
+                }
             }
-        }
 
-        tokenizer_type energy_deviation_tokenizer(energy_deviation_value,
-                                                  predicate,
-                                                  strtk::tokenize_options::compress_delimiters);
+            ///////////////////////////////////////////////////////////////////////////////////////
+            // Homogeneous Texture.
+            ///////////////////////////////////////////////////////////////////////////////////////
 
-        itr = energy_deviation_tokenizer.begin();
+            if (!found_average) {
+                pos = line.find("<Average>");
 
-        while (itr != energy_deviation_tokenizer.end()) {
-            if (itr.as_string() != "") {
-                input << itr;
-                input << " ";
+                if (pos != string::npos) {
+                    input << line.substr(9, line.length() - 19);
+                    input << " ";
+
+                    found_average = true;
+
+                    continue;
+                }
             }
 
-            ++itr;
+            if (!found_standard_deviation) {
+                pos = line.find("<StandardDeviation>");
+
+                if (pos != string::npos) {
+                    input << line.substr(19, line.length() - 39);
+                    input << " ";
+
+                    found_standard_deviation = true;
+
+                    continue;
+                }
+            }
+
+            if (!found_energy) {
+                pos = line.find("<Energy>");
+
+                if (pos != string::npos) {
+                    string value = line.substr(8, line.length() - 17);
+
+                    tokenizer_type tokenizer(value,
+                                             predicate,
+                                             strtk::tokenize_options::compress_delimiters);
+
+                    tokenizer_type::iterator itr = tokenizer.begin();
+
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
+
+                        ++itr;
+                    }
+
+                    found_energy = true;
+
+                    continue;
+                }
+            }
+
+            if (!found_energy_deviation) {
+                pos = line.find("<EnergyDeviation>");
+
+                if (pos != string::npos) {
+                    string value = line.substr(17, line.length() - 35);
+
+                    tokenizer_type tokenizer(value,
+                                             predicate,
+                                             strtk::tokenize_options::compress_delimiters);
+
+                    tokenizer_type::iterator itr = tokenizer.begin();
+
+                    while (itr != tokenizer.end()) {
+                        if (itr.as_string() != "") {
+                            input << itr;
+                            input << " ";
+                        }
+
+                        ++itr;
+                    }
+
+                    found_energy_deviation = true;
+
+                    continue;
+                }
+            }
         }
 
         input << endl;
